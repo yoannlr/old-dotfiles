866e2cee8a75518f23aa3682b18340d1a498c20a
yoannlr <yoann22lr@gmail.com>
2020-04-20 23:45:59 +0200

    Improved battery script

--- .scripts/battery.sh
+++ .scripts/battery.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
+# $1 : power source (usually BAT0)
+# $2 : demay between each check
+# $3 : low battery treshold
+
+statfile="/sys/class/power_supply/$1/status"
+capfile="/sys/class/power_supply/$1/capacity"
+
+delay=$2
+treshold=$3
 notified=0
-status=$(cat /sys/class/power_supply/BAT0/status)
-charge_now=$(cat /sys/class/power_supply/BAT0/charge_now)
-charge_full=$(cat /sys/class/power_supply/BAT0/charge_full)
-percent=$((charge_now / charge_full))
+stat=$(cat "$statfile")
+cap=$(cat "$capfile")
 
 while /bin/true
 do
-	status=$(cat /sys/class/power_supply/BAT0/status)
-	if [ "$status" = "Discharging" ]
+	stat=$(cat "$statfile")
+	if [ "$stat" = "Discharging" ]
 	then
-		charge_now=$(cat /sys/class/power_supply/BAT0/charge_now)
-		charge_full=$(cat /sys/class/power_supply/BAT0/charge_full)
-		percent=$((charge_now * 100 / charge_full))
-		echo $percent
-		if [ $percent -lt 10 ] && [ ! $notified -eq 1 ]
+		cap=$(cat "$capfile")
+		if [ $cap -lt $treshold ] && [ ! $notified -eq 1 ]
 		then
 			notify-send -u critical 'Low battery'
 			mpv $SCRIPTS/lowbattery.m4a &
@@ -24,5 +28,5 @@ do
 	else
 		notified=0
 	fi
-	sleep 10
+	sleep $delay
 done
