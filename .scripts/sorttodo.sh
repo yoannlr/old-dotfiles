015f0c5ba34228516c9d3118b9671abcda7099d8
yoannlr <yoann22lr@gmail.com>
2020-05-05 22:55:11 +0200

    Todolist files script

---
+++ .scripts/sorttodo.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# this script is called by vim when saving a *.todo file (therefore, it requires some config in vimrc)
+# it will add the date and time for the completed tasks
+# and put the completed tasks at the end of the file
+
+todofile="$1"
+
+sed -r -i "s/^# ([A-Za-z0-9])/# \[$(date '+%Y-%m-%d %H:%M')\] \1/g" "$todofile"
+sort -r -o "$todofile" "$todofile"
--- .vimrc
+++ .vimrc
@@ -6,3 +6,5 @@ set timeoutlen=1000 ttimeoutlen=0
 map te :tabe<Space>
 map tp :tabp<Return>
 map tn :tabn<Return>
+
+autocmd BufWritePost *.todo !sorttodo.sh %
