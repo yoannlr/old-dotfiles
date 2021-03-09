f977185a998c1efe8d3408c1ec53983e3d650de0
yoannlr <yoann22lr@gmail.com>
2021-03-09 11:29:36 +0100

    .screenlayout moved to .local/screenlayout

--- sync.sh
+++ sync.sh
@@ -3,6 +3,7 @@
 # this script is only used to synchronize the files in use to the local repository
 
 cp -vr ~/.local/bin/* .local/bin/
+cp -vr ~/.local/screenlayout/* .local/screenlayout/
 cp -v ~/.config/i3blocks/config .config/i3blocks/config
 cp -v ~/.config/i3/config .config/i3/config
 cp -v ~/.config/i3status/config .config/i3status/config
@@ -14,7 +15,6 @@ cp -v ~/.aliasrc .aliasrc
 cp -v ~/.vimrc .vimrc
 cp -v ~/.xinitrc .xinitrc
 cp -v ~/.profile .profile
-cp -v ~/.screenlayout/* .screenlayout/
 cp -v ~/.config/sxiv/exec/key-handler .config/sxiv/exec/
 cp -v ~/.config/Thunar/uca.xml .config/Thunar/
 cp -v ~/.config/alacritty.yml .config/alacritty.yml
