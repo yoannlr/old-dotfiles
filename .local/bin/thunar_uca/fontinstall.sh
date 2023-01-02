7982ff077257dfb0caba5dd963acc3fa8b644502
yoannlr <yoann22lr@gmail.com>
2023-01-02 19:03:53 +0100

    Dotfiles update - 2023-01-02.

--- .local/bin/thunar_uca/fontinstall.sh
+++ .local/bin/thunar_uca/fontinstall.sh
@@ -4,7 +4,7 @@ FONTSDIR="$HOME/.local/share/fonts"
 
 [ ! -d "$FONTSDIR" ] && mkdir "$FONTSDIR"
 
-for arg in $@
+for arg in "$@"
 do
 	cp "$arg" "$FONTSDIR/"
 done
--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo ":: Simulating deploying dotfiles to $HOME"
-rsync --dry-run -v -r -lp --exclude={'README.md','sync.sh','deploy.sh','.git','programs.csv','screenshot.png','install.sh'} "$PWD/" $HOME
-read -p ":: Is that correct? [Y/n]" cont
-[ "$cont" = "n" ] && echo ":: Abort." && exit
-rsync -v -r -lp --exclude={'README.md','sync.sh','deploy.sh','.git','programs.csv','screenshot.png','install.sh'} "$PWD/" $HOME
-echo ":: Done deploying dotfiles"
-
